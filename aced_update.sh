00ca8c7b27a0f68dcb49931c2cbd626f791fa260
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-08-12 18:06:37 -0500

    Update aced_update.sh

--- aced_update.sh
+++ aced_update.sh
@@ -22,6 +22,7 @@ function update_sentinel() {
 function update_node() {
   echo -e "Preparing to download updated $COIN_NAME"
   rm /usr/local/bin/aced*
+  rm ~/.acedcore/peers.dat
   cd $TMP_FOLDER
   wget -q $COIN_REPO
   compile_error
