de2ee890b7fe169a52a103c63c1e97dbfe54bc1b
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-08-13 22:02:15 -0500

    Update aced_update.sh

--- aced_update.sh
+++ aced_update.sh
@@ -89,7 +89,7 @@ function import_bootstrap() {
   wget -q $COIN_BS
   compile_error
   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
-  unzip $COIN_ZIP >/dev/null 2>&1
+  tar -xvf $COIN_ZIP >/dev/null 2>&1
   compile_error
   cp -r ~/bootstrap/blocks ~/.acedcore/blocks
   cp -r ~/bootstrap/chainstate ~/.acedcore/chainstate
